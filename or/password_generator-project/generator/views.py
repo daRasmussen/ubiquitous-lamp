73459ec6dec184a8c6e96049a8a7e0d7049a3566
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-10-27 16:48:25 +0200

    adds some simple views

--- or/password_generator-project/generator/views.py
+++ or/password_generator-project/generator/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(req):
+    return HttpResponse("Hello World/")
+
+
+def eggs(req):
+    return HttpResponse("Eggs är great! ")

aaae692a5bf3c4f32e6160c9d5e550528e1bf35a
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-10-27 20:53:55 +0200

    adds a working blog

--- or/personal_portfolio_project/blog/admin.py
+++ or/personal_portfolio_project/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Blog
+
+""" I want to see this model inside the admin """
+admin.site.register(Blog)
--- or/personal_portfolio_project/blog/views.py
+++ or/personal_portfolio_project/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
 
 
 # Create your views here.
+# noinspection PyUnresolvedReferences
 def all_blogs(req):
-    return render(req, 'blog/all_blogs.html')
+    posts = Blog.objects.order_by('-date')[:1]
+    return render(req, 'blog/all_blogs.html', {'posts': posts})

79853967f758fe13f2fc48d8caf7717cabb7528f
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-10-27 20:20:22 +0200

    adds blog

--- or/personal_portfolio_project/personal_portfolio/urls.py
+++ or/personal_portfolio_project/personal_portfolio/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="nome")
+    path('', views.home, name="nome"),
+    path('blog/', include('blog.urls')),
 ]
 """ Add Static files, media files. """
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

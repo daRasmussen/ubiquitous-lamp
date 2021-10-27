38160094fc44040b361a0ac79db10d0ebae5753b
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-10-27 19:55:00 +0200

    fixes home.html

--- or/personal_portfolio_project/personal_portfolio/urls.py
+++ or/personal_portfolio_project/personal_portfolio/urls.py
@@ -18,8 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from portfolio import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="nome")
 ]
 """ Add Static files, media files. """
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

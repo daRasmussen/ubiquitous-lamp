7ae462ecfc4141d4c0a236142d30c9ff4b951034
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-10-29 03:46:07 +0200

    adds intial views

--- drgeo/blocket-project/main/views.py
+++ drgeo/blocket-project/main/views.py
@@ -2,10 +2,13 @@ from enum import Enum
 
 from django.shortcuts import render
 
+class Names(str, Enum):
+    APP = 'Blocket'
+
 
 class Locations(str, Enum):
     INDEX = 'main/index.html'
 
 
 def index(req):
-    return render(req, Locations.INDEX)
+    return render(req, Locations.INDEX, {'title': f"{Names.APP}"})

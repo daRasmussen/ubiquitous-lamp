410d121b59ccfcd8e3ad74d18e5ffff7dcc2d426
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-10-31 02:48:47 +0200

    adds a command line util for faker

---
+++ drgeo/blocket-project/account/management/commands/createdata.py
@@ -0,0 +1,32 @@
+import faker.providers
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+
+CATEGORIES = [
+    "Shoes",
+    "Shirts"
+]
+
+PRODUCTS = [
+    "Shoes",
+    "Shirts"
+]
+
+
+class Provider(faker.providers.BaseProvider):
+    def ecommerce_category(self):
+        return self.random_element(CATEGORIES)
+
+    def ecommerce_products(self):
+        return self.random_element(PRODUCTS)
+
+
+class Command(BaseCommand):
+    help = "Command information"
+
+    def handle(self, *args, **kwargs):
+        fake = Faker(["sv_SE"])
+        print(fake.address())
+        fake.add_provider(Provider)
+        print(fake.ecommerce_products())

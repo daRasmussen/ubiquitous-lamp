b14a20e71ac3ff7bcd58126d52ceb540ec82c583
Daniel Rasmussen <daniel.rasmussen@drgeo.se>
2021-10-31 21:15:52 +0100

    adds tests for forms in account

--- drgeo/blocket-project/account/tests/test_forms.py
+++ drgeo/blocket-project/account/tests/test_forms.py
@@ -1,17 +1,40 @@
 from django.test import SimpleTestCase
+from faker import Faker
 
 from account.forms import SignUpForm
 
 
 class TestForms(SimpleTestCase):
+    profiles = []
+
+    def setUp(self):
+        self.fakers = [
+            Faker(["sv_SE"]),
+            Faker(["da_DK"]),
+            Faker(["en_US"]),
+            Faker(["bg_BG"]),
+            Faker(["da_DK"]),
+            Faker(["sv_SE"]),
+            Faker(["bg_BG"]),
+            Faker(["en_US"]),
+        ]
+        for fake in self.fakers:
+            for _ in range(100):
+                profile = fake.profile()
+                self.profiles.append({
+                    "username": profile["username"],
+                    "email": profile["mail"],
+                    "password": fake.password(length=12)
+                })
+
     def test_signup_form_valid_data(self):
-        form = SignUpForm(data={
-            "email": "user@blocket.se",
-            "username": "user",
-            "password1": "password",
-            "password2": "password"
-        })
-        self.assertTrue(form.is_valid())
+        for profile in self.profiles:
+            assert SignUpForm(data={
+                "email": profile["email"],
+                "username": profile["username"],
+                "password1": profile["password"],
+                "password2": profile["password"]
+            }).is_valid()
 
     def test_signup_form_no_data(self):
         form = SignUpForm(data={})
